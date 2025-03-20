288d2493a33c29c536ac4c24688886fa2088c222
gnouvdev <quocvuong132003@gmail.com>
2025-03-20 19:49:18 +0700

    update order

--- index.js
+++ index.js
@@ -1,33 +1,36 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+
 const mongoose = require("mongoose");
 const port = process.env.PORT || 5000;
-require("dotenv").config();
+require('dotenv').config()
 
-//midleware
+// middleware
 app.use(express.json());
-app.use(
-  cors({
-    origin: ["http://localhost:5173"],
-    credentials: true,
-  })
-);
+app.use(cors({
+    origin: ['http://localhost:5173', 'https://book-app-frontend-tau.vercel.app'],
+    credentials: true
+}))
 
-//routes
-const bookRoutes = require("./src/books/book.route");
-app.use("/api/books", bookRoutes);
-//5CFQOkI6dkBYON35
+// routes
+const bookRoutes = require('./src/books/book.route');
+const orderRoutes = require("./src/orders/order.route")
+
+
+app.use("/api/books", bookRoutes)
+app.use("/api/orders", orderRoutes)
+
 
 async function main() {
   await mongoose.connect(process.env.DB_URL);
   app.use("/", (req, res) => {
-    res.send("Book Store Server is runing !");
+    res.send("Book Store Server is running!");
   });
 }
-main()
-  .then(console.log("MongoDb connect Succesully"))
-  .catch((err) => console.log(err));
+
+main().then(() => console.log("Mongodb connect successfully!")).catch(err => console.log(err));
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
